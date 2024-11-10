0cf7183904013ce40f2fe318b4af4b8dd647afdf
Mahmoud <isamir@abuerdan.com>
2024-11-10 13:57:10 +0200

    first commit

---
+++ core/scripts/orm_script.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from core.models import Restaurant, Rating, Sale
+from django.utils import timezone
+from django.db import connection
+from pprint import pprint
+
+def run():
+    pprint(connection.queries)
